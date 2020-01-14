c669d96e58707ca2d447635afc4e5d6bcad35fc1
Maya <jaffe.maya@gmail.com>
2020-01-14 09:53:34 -0500

    adding proper chart code

--- modules/chart.R
+++ modules/chart.R
@@ -1,20 +1,20 @@
 chart <- function(input, output, session, datafile = reactive(NULL)) {
-
-  # SINCE DATAFILE IS A REACTIVE WE ADD THE PRERENTHESIS HERE
-  # WHERE/HOW CAN I ACCESS input$xaxis?
-  # Do I need to use ns? Can you do that in the server side of a module?
-  output$XAXIS <- renderUI(selectInput("xaxis", "X Axis", choices = colnames(datafile())))
-  output$YAXIS <- renderUI(selectInput("yaxis", "Y Axis", choices = colnames(datafile())))
   
-  # NOT WORKING
-  # Use the selectInput x and y to plot
+
+  # In module UI, we usually use NS function to add the namespace in front of the output id. 
+  # But in module server, the main server use callModule to the call the module server 
+  # and the function will automatically add the namespace to all input and output id. 
+  # But for renderUI in chart, the selectInput behave like the module UI, 
+  # you need to add the namespace by yourself, so use session$ns to add the namespace 
+  # to the newly added input id in order to match with the input xaxis and yaxis.
+  output$XAXIS <- renderUI(selectInput(session$ns("xaxis"), "X Axis", choices = colnames(datafile())))
+  output$YAXIS <- renderUI(selectInput(session$ns("yaxis"), "Y Axis", choices = colnames(datafile())))
+  
   p <- reactive({
     req(datafile)
-    # WORKS: ggplot(datafile(), aes(x = Sepal_Length, y = Sepal_Width))
-    # DOES NOT WORK:
     ggplot(datafile(), aes_(x = as.name(input$xaxis), y = as.name(input$yaxis))) +
       geom_point()
   })
-
+  
   return(p)
 }
